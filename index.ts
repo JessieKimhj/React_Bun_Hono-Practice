e08e5a620929641d02539e77b350a423095693e9
JessieKimhj <dpro5893@gmail.com>
2025-03-11 02:45:19 -0700

    installed hono, added logger

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-console.log("Hello via Bun!");
+import app from "./app";
+
+const port = process.env.PORT || 3000
+Bun.serve({
+    fetch(req) {
+      return new Response("Hello from bun server!");
+    },
+  })
+
+  console.log("server!");
